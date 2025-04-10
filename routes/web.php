d2c52eb8bbe4a1b09fe9c7b2982bfb982a5908b8
thihaswe.work <thihaswe.work@gmail.com>
2025-04-10 15:10:37 +0630

    jetstream login/logout

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+Route::get('redirect',[HomeController::class,'redirect']);
